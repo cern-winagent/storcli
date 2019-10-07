990f57447413e8e0e1592d3c811c599c9a20c23a
Esteban Puentes <esteban.puentes@cern.ch>
2019-10-07 17:03:44 +0200

    Add messaging support between plugins and the agent

--- storcli/IStorcli.cs
+++ storcli/IStorcli.cs
@@ -19,6 +19,8 @@ namespace storcli
         private Server Server;
         private Settings.Plugin settings;
 
+        public event EventHandler<MessageEventArgs> MessageEvent;
+
         public string Execute(JObject set)
         {
             settings = set.ToObject<Settings.Plugin>();
